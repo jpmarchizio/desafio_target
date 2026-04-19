451ceff0c827aa4b6883e715e0b1a4c80dab4222
João Marchizio <jpmarchizio@gmail.com>
2026-04-19 20:52:53 -0300

    feat: auth - firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
+import 'package:desafio_target/core/di/injector.dart';
+import 'package:desafio_target/firebase_options.dart';
 import 'package:desafio_target/target_to_do.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
-import 'core/di/injector.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await setupInjector();
 
   runApp(const TargetToDoApp());
